cde7a164eb4c9553780e79f3a6000fa14f5d5909
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2020-12-19 15:40:50 -0600

    clean up code, remove typing effect, remove resume link

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,36 +1,36 @@
 /*open menu on click*/
 openFullMenu = () => {
-  document.getElementById("fullMenu").classList.toggle("hidden");
-  document.getElementById("closeButton").classList.toggle("hidden");
-  document.getElementById("openButton").classList.toggle("hidden");
+  document.getElementById('fullMenu').classList.toggle('hidden');
+  document.getElementById('closeButton').classList.toggle('hidden');
+  document.getElementById('openButton').classList.toggle('hidden');
 };
 
 /*smooth scrolling to each section*/
-scrollToAbout = className => {
-  const elementList = document.querySelectorAll("." + className);
+scrollToAbout = (className) => {
+  const elementList = document.querySelectorAll('.' + className);
   const element = elementList[0];
-  element.scrollIntoView({ behavior: "smooth" });
+  element.scrollIntoView({ behavior: 'smooth' });
   openFullMenu();
 };
 
-scrollToTop = className => {
-  const elementList = document.querySelectorAll("." + className);
+scrollToTop = (className) => {
+  const elementList = document.querySelectorAll('.' + className);
   const element = elementList[0];
-  element.scrollIntoView({ behavior: "smooth" });
+  element.scrollIntoView({ behavior: 'smooth' });
 };
 
 /*define canvas and other elements sizes*/
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const c = canvas.getContext("2d");
+const c = canvas.getContext('2d');
 
-let colors = ["#dc143c", "#75afa8", "#d9b449", "#008080"];
+let colors = ['#dc143c', '#75afa8', '#d9b449', '#008080'];
 
 /*re-run animation on resize of canvas to account for new size*/
-addEventListener("resize", () => {
+addEventListener('resize', () => {
   canvas.height = innerHeight;
   canvas.width = innerWidth;
 
@@ -48,7 +48,7 @@ function findDistance(x1, y1, x2, y2) {
 function rotate(velocity, angle) {
   const rotatedVelocities = {
     x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle),
   };
 
   return rotatedVelocities;
@@ -64,10 +64,7 @@ function collision(particle, otherParticle) {
   // Prevent accidental overlap of particles
   //Diff in velocity and distance of the two particles that are being passed through will only react when this is = 0
   if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-    const angle = -Math.atan2(
-      otherParticle.y - particle.y,
-      otherParticle.x - particle.x
-    );
+    const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
 
     const m1 = particle.mass;
     const m2 = otherParticle.mass;
@@ -80,11 +77,11 @@ function collision(particle, otherParticle) {
 
     const v1 = {
       x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
-      y: u1.y
+      y: u1.y,
     };
     const v2 = {
       x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
-      y: u2.y
+      y: u2.y,
     };
 
     // Final velocity after rotating axis back to original location
@@ -106,36 +103,28 @@ function Circle(x, y, radius, color) {
   this.y = y;
   this.velocity = {
     x: (Math.random() - 0.5) * 1.5,
-    y: (Math.random() - 0.5) * 1.5
+    y: (Math.random() - 0.5) * 1.5,
   };
   this.radius = radius;
   this.color = color;
   this.mass = 1;
   this.opacity = 0;
 
-  this.update = circleArray => {
+  this.update = (circleArray) => {
     this.draw();
 
     for (let i = 0; i < circleArray.length; i++) {
       //way to not included itself
       if (this === circleArray[i]) continue;
-      if (
-        findDistance(this.x, this.y, circleArray[i].x, circleArray[i].y) -
-          this.radius * 2 <
-        0
-      ) {
+      if (findDistance(this.x, this.y, circleArray[i].x, circleArray[i].y) - this.radius * 2 < 0) {
         collision(this, circleArray[i]);
       }
 
-      if (
-        findDistance(this.x, this.y, circleArray[i].x, circleArray[i].y) -
-          this.radius * 2 <
-        50
-      ) {
+      if (findDistance(this.x, this.y, circleArray[i].x, circleArray[i].y) - this.radius * 2 < 50) {
         c.save();
         c.beginPath();
-        c.strokeStyle = "rgba(255, 255, 255, 0.3)";
-        c.shadowColor = "#e3eaef";
+        c.strokeStyle = 'rgba(255, 255, 255, 0.3)';
+        c.shadowColor = '#e3eaef';
         c.lineWidth = 0.3;
         c.moveTo(this.x, this.y);
         c.lineTo(circleArray[i].x, circleArray[i].y);
@@ -159,11 +148,11 @@ function Circle(x, y, radius, color) {
   };
 
   // //define the circles that are drawn
-  this.draw = function() {
+  this.draw = function () {
     c.save();
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.shadowColor = "#e3eaef";
+    c.shadowColor = '#e3eaef';
     c.shadowBlur = 20;
     c.fillStyle = this.color;
     c.fill();
@@ -204,11 +193,11 @@ function Sun(x, y, radius, color) {
   this.radius = radius;
   this.color = color;
 
-  this.update = function() {
+  this.update = function () {
     this.draw();
   };
 
-  this.draw = function() {
+  this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     c.fillStyle = this.color;
@@ -223,11 +212,11 @@ function Orbit(x, y, radius, color) {
   this.radius = radius;
   this.color = color;
 
-  this.update = function() {
+  this.update = function () {
     this.draw();
   };
 
-  this.draw = function() {
+  this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     //c.fillStyle = this.color
@@ -271,7 +260,7 @@ function init() {
   const planet7dist = canvas.width / 2.5;
   const planet8dist = canvas.width / 2.2;
 
-  let colors = ["#dc143c", "#75afa8", "#d9b449", "#008080"];
+  let colors = ['#dc143c', '#75afa8', '#d9b449', '#008080'];
 
   if (canvas.width >= 800) {
     let newY = allY - 150;
@@ -288,14 +277,14 @@ function init() {
     planet7 = new Planet(newX, newY, 32, colors[2], 0.00228, planet7dist);
     planet8 = new Planet(newX, newY, 28, colors[3], 0.00182, planet8dist);
 
-    orbit1 = new Orbit(newX, newY, planet1dist, "black");
-    orbit2 = new Orbit(newX, newY, planet2dist, "black");
-    orbit3 = new Orbit(newX, newY, planet3dist, "black");
-    orbit4 = new Orbit(newX, newY, planet4dist, "black");
-    orbit5 = new Orbit(newX, newY, planet5dist, "black");
-    orbit6 = new Orbit(newX, newY, planet6dist, "black");
-    orbit7 = new Orbit(newX, newY, planet7dist, "black");
-    orbit8 = new Orbit(newX, newY, planet8dist, "black");
+    orbit1 = new Orbit(newX, newY, planet1dist, 'black');
+    orbit2 = new Orbit(newX, newY, planet2dist, 'black');
+    orbit3 = new Orbit(newX, newY, planet3dist, 'black');
+    orbit4 = new Orbit(newX, newY, planet4dist, 'black');
+    orbit5 = new Orbit(newX, newY, planet5dist, 'black');
+    orbit6 = new Orbit(newX, newY, planet6dist, 'black');
+    orbit7 = new Orbit(newX, newY, planet7dist, 'black');
+    orbit8 = new Orbit(newX, newY, planet8dist, 'black');
   } else if (canvas.width < 800 && canvas.width > 600) {
     let newY = allY - 150;
     let newX = allX + 75;
@@ -311,14 +300,14 @@ function init() {
     planet7 = new Planet(newX, newY, 23, colors[2], 0.00228, planet7dist);
     planet8 = new Planet(newX, newY, 20, colors[3], 0.00182, planet8dist);
 
-    orbit1 = new Orbit(newX, newY, planet1dist, "black");
-    orbit2 = new Orbit(newX, newY, planet2dist, "black");
-    orbit3 = new Orbit(newX, newY, planet3dist, "black");
-    orbit4 = new Orbit(newX, newY, planet4dist, "black");
-    orbit5 = new Orbit(newX, newY, planet5dist, "black");
-    orbit6 = new Orbit(newX, newY, planet6dist, "black");
-    orbit7 = new Orbit(newX, newY, planet7dist, "black");
-    orbit8 = new Orbit(newX, newY, planet8dist, "black");
+    orbit1 = new Orbit(newX, newY, planet1dist, 'black');
+    orbit2 = new Orbit(newX, newY, planet2dist, 'black');
+    orbit3 = new Orbit(newX, newY, planet3dist, 'black');
+    orbit4 = new Orbit(newX, newY, planet4dist, 'black');
+    orbit5 = new Orbit(newX, newY, planet5dist, 'black');
+    orbit6 = new Orbit(newX, newY, planet6dist, 'black');
+    orbit7 = new Orbit(newX, newY, planet7dist, 'black');
+    orbit8 = new Orbit(newX, newY, planet8dist, 'black');
   } else if (canvas.width <= 600) {
     let newY = allY - 150;
     let newX = allX + 75;
@@ -334,14 +323,14 @@ function init() {
     planet7 = new Planet(newX, newY, 14.5, colors[2], 0.00228, planet7dist);
     planet8 = new Planet(newX, newY, 11, colors[3], 0.00182, planet8dist);
 
-    orbit1 = new Orbit(newX, newY, planet1dist, "black");
-    orbit2 = new Orbit(newX, newY, planet2dist, "black");
-    orbit3 = new Orbit(newX, newY, planet3dist, "black");
-    orbit4 = new Orbit(newX, newY, planet4dist, "black");
-    orbit5 = new Orbit(newX, newY, planet5dist, "black");
-    orbit6 = new Orbit(newX, newY, planet6dist, "black");
-    orbit7 = new Orbit(newX, newY, planet7dist, "black");
-    orbit8 = new Orbit(newX, newY, planet8dist, "black");
+    orbit1 = new Orbit(newX, newY, planet1dist, 'black');
+    orbit2 = new Orbit(newX, newY, planet2dist, 'black');
+    orbit3 = new Orbit(newX, newY, planet3dist, 'black');
+    orbit4 = new Orbit(newX, newY, planet4dist, 'black');
+    orbit5 = new Orbit(newX, newY, planet5dist, 'black');
+    orbit6 = new Orbit(newX, newY, planet6dist, 'black');
+    orbit7 = new Orbit(newX, newY, planet7dist, 'black');
+    orbit8 = new Orbit(newX, newY, planet8dist, 'black');
   }
 
   circleArray = [];
@@ -349,28 +338,16 @@ function init() {
   function makeCircles(circlesAmount) {
     for (let i = 0; i < circlesAmount; i++) {
       const radius = 2;
-      let x = Math.floor(
-        Math.random() * (canvas.width - radius - radius + 1) + radius
-      );
-      let y = Math.floor(
-        Math.random() * (canvas.height - radius - radius + 1) + radius
-      );
-      let color = "white"; //colors[Math.floor(Math.random() * colors.length)];
+      let x = Math.floor(Math.random() * (canvas.width - radius - radius + 1) + radius);
+      let y = Math.floor(Math.random() * (canvas.height - radius - radius + 1) + radius);
+      let color = 'white'; //colors[Math.floor(Math.random() * colors.length)];
 
       if (i !== 0) {
         for (let j = 0; j < circleArray.length; j++) {
-          if (
-            findDistance(x, y, circleArray[j].x, circleArray[j].y) -
-              radius * 2 <
-            0
-          ) {
+          if (findDistance(x, y, circleArray[j].x, circleArray[j].y) - radius * 2 < 0) {
             //if they are then generate new x and y
-            x = Math.floor(
-              Math.random() * (canvas.width - radius - radius + 1) + radius
-            );
-            y = Math.floor(
-              Math.random() * (canvas.height - radius - radius + 1) + radius
-            );
+            x = Math.floor(Math.random() * (canvas.width - radius - radius + 1) + radius);
+            y = Math.floor(Math.random() * (canvas.height - radius - radius + 1) + radius);
 
             //start the loop over to make sure new particles generated aren't overlapping
             j = -1;
@@ -396,13 +373,10 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
 
-  // c.fillStyle = backgroundGradient
-  // c.fillRect(0, 0, canvas.width, canvas.height)
-
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   //draw the circles on the screen
-  circleArray.forEach(circle => {
+  circleArray.forEach((circle) => {
     circle.update(circleArray);
   });
 
@@ -425,9 +399,6 @@ function animate() {
   planet6.update();
   planet7.update();
   planet8.update();
-
-  // c.fillStyle = grd;
-  // c.fillRect(0, 0, canvas.width, canvas.height)
 }
 
 init();
